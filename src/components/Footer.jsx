45b4cbf425ee581c70f29b5935b12b6f4af01a36
sachinraj16 <iamsachin1300@gmail.com>
2024-02-02 23:22:25 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechSolution</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.youtube.com/channel/UCfcdpugbOC-QIcxZUZEijhA "
+            target="blank"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sachin-raj-91659a197/"
+            target="blank"
+          >
+            Instagram
+          </a>
+          <a href=" https://github.com/sachinraj16" target="blank">
+            GitHub
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
